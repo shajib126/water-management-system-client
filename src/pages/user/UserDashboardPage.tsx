0b8092b6cae3f8c0f67f7742313ca356f499b594
shajib126 <shajib126@gmail.com>
2024-03-30 12:40:59 +0600

    logout implement in customer profile

--- src/pages/user/UserDashboardPage.tsx
+++ src/pages/user/UserDashboardPage.tsx
@@ -1,5 +1,5 @@
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../redux/hooks'
 import { logout } from '../../redux/features/auth/authSlice'
 
@@ -24,9 +24,14 @@ const menu = [
 
 const UserDashboardPage = () => {
   const dispatch = useAppDispatch()
+const navigate = useNavigate()
+  const handleLogout = ()=>{
+    dispatch(logout())
+    navigate('/login')
+  }
   return (
     <div className=''>
-      <button onClick={()=>dispatch(logout())} className='btn btn-warning absolute top-0 right-4 top-[7%]'>Logout</button>
+      <button onClick={handleLogout} className='btn btn-warning absolute top-0 right-4 top-[7%]'>Logout</button>
      <ul className="bg-lime-500 rounded-md w-full menu menu-horizontal bg-base-200 ">
         {menu.map((item,i)=>(
           <li key={i} className="border-2 rounded-md m-2 text-teal-950 font-bold text-[14px]">
